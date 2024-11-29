2de2e4540363b3ffc9837a400d518fbc0dd8ce47
Jakob Heuser <jakob@codedrift.com>
2024-11-29 09:48:19 -0800

    feat: Adds Soridata sync support

--- scripts/mysql2db.ts
+++ scripts/mysql2db.ts
@@ -1,3 +1,6 @@
+// required first import
+import "dotenv/config.js";
+
 import { dirname, resolve } from "path";
 import {
   existsSync,
@@ -14,17 +17,44 @@ import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 import { mysql2sqlite } from "../src/vendor/mysql2sqlite.js";
 
+// do not attempt to fetch without a valid soridata key,
+// or you will get banned you've been warned.
+const SORIDATA_KEY = process.env.SORIDATA_KEY;
+const SORIDATA_CONTACT = process.env.SORIDATA_CONTACT;
+const SORIDATA_URL = "https://soridata.com/download.php";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const tmpdir = resolve(__dirname, "../tmp");
 const targetdir = resolve(__dirname, "../db");
+const soridataAlreadyExists = existsSync(resolve(tmpdir, "dbdownload.zip"));
 
 // mkdirp the tmpdir
 mkdirpSync(tmpdir);
 mkdirpSync(targetdir);
 
-// TODO, use soridata permanent download URL
-// downloads file into ./tmp
-// until we have the URL, place dbdownload.zip into the tmpdir manually
+// If you do not have a soridata key, just download the database manually
+// seriously. It is free. Don't ruin it for others.
+// https://soridata.com/en/database_download.html
+if (!soridataAlreadyExists && SORIDATA_KEY && SORIDATA_CONTACT) {
+  console.log("Downloading Soridata");
+  const fd = new FormData();
+  fd.set("safety", "");
+  fd.set("pass", SORIDATA_KEY ?? "");
+
+  const headers = new Headers();
+  headers.set("User-Agent", `bias.is (${SORIDATA_CONTACT})`);
+
+  const zipResp = await fetch(SORIDATA_URL, {
+    method: "POST",
+    headers,
+    body: fd,
+  });
+
+  const data = await zipResp.arrayBuffer();
+  writeFileSync(resolve(tmpdir, "dbdownload.zip"), Buffer.from(data));
+} else if (soridataAlreadyExists) {
+  console.log("Soridata exists. Not downloading again.");
+}
 
 // check if dbdownload.zip exists
 if (!existsSync(resolve(tmpdir, "dbdownload.zip"))) {
