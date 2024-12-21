6e8554bd0fbd97e3c5b573525cdfb67ecd24ca5f
Jakob Heuser <jakob@codedrift.com>
2024-12-20 16:19:50 -0800

    wip: Adds suggestions from querying labeler db

--- src/actions/admin/suggest.ts
+++ src/actions/admin/suggest.ts
@@ -4,12 +4,14 @@ import { selectBias } from "../../commonSql/selectBias.js";
 import { rowToLabel } from "../add.js";
 import { toIdentifier } from "../../util/toIdentifier.js";
 import { doList } from "../list.js";
+import { db } from "../../db.js";
+import { ComAtprotoLabelDefs } from "@atcute/client/lexicons";
 
 // search to single record to identifier
 // get random entry from sqllite with the label enabled (1)
 
-// /suggest ateez = results limited to ateez
-// /suggest = suggest of some bias overlap
+// /admin suggest ateez = results limited to ateez
+// /admin suggest = suggest of some bias overlap
 export const suggest: AdminActionHandler = async (
   message,
   conversation,
@@ -30,11 +32,43 @@ export const suggest: AdminActionHandler = async (
     identifiers.push(...bias.map((b) => b.val), ...ult.map((b) => b.val));
   }
 
+  // order by created ASC
+  const stmt = await db.prepare(`
+    SELECT * FROM labels
+    WHERE val IN ('${identifiers.map(ident => ident).join("', '")}')
+    ORDER BY cts DESC
+    LIMIT 100
+  `);
+  const rows = await stmt.all<ComAtprotoLabelDefs.Label[]>();
+
+  const fans: Record<string, Set<string>> = {};
+  for (const row of rows.reverse()) {
+    const label = row.val;
+    const fan = row.uri;
+    const negate = row.neg;
+
+    if (!fans[fan]) {
+      fans[fan] = new Set();
+    }
+
+    if (negate) {
+      fans[fan].delete(label);
+    } else {
+      fans[fan].add(label);
+    }
+  }
+
+  const debug = Object.entries(fans).map(([fan, dids]) => {
+    return `${fan}: ${Array.from(dids).join(", ")}`;
+  });
+
   await conversation.sendMessage({
     text: dedent`
       ${message.text}
       Suggestions for ${message.senderDid}:
       ${identifiers.join(", ")}
+      ---
+      ${debug.join("\n")}
     `,
   });
 };
