da311b8c7b45d71d31a7e04109174bd7909c55f6
Jakob Heuser <jakob@codedrift.com>
2024-12-20 17:48:00 -0800

    feat: New suggestion endpoint (unwired)

--- src/actions/admin/suggest.ts
+++ src/actions/admin/suggest.ts
@@ -1,15 +1,7 @@
-import dedent from "dedent";
 import { AdminActionHandler } from "./types.js";
-import { selectBias } from "../../commonSql/selectBias.js";
-import { nameToUlt, rowToLabel, ultToName } from "../add.js";
-import { toIdentifier } from "../../util/toIdentifier.js";
-import { doList } from "../list.js";
-import { db } from "../../db.js";
-import { At, ComAtprotoLabelDefs } from "@atcute/client/lexicons";
+import { At } from "@atcute/client/lexicons";
 import { RichText } from "@skyware/bot";
-import { getLabelerLabelDefinitions } from "@skyware/labeler/scripts";
-import { DID, LABELER_PASSWORD } from "../../constants.js";
-import { DidDocument } from "@atcute/client/utils/did";
+import { doSuggest } from "../suggest.js";
 
 // search to single record to identifier
 // get random entry from sqllite with the label enabled (1)
@@ -21,111 +13,20 @@ export const suggest: AdminActionHandler = async (
   conversation,
   options
 ) => {
-  const identifiers: string[] = [];
+  const suggestion = await doSuggest(message.senderDid, options?.arguments);
 
-  if (options?.arguments) {
-    const result = await selectBias(options.arguments);
-    identifiers.push(
-      toIdentifier(rowToLabel(result).name),
-      toIdentifier(rowToLabel(result, true).name)
-    );
-  } else {
-    // use users bias/ult list
-    // the "ult" version comes from the labeler, where we convert the en
-    // name to back to an identifier, turning the emoji back into dashes
-    // like "💖 ATEEZ" -> "--ATEEZ"
-    const { bias, ult } = await doList(message.senderDid);
-    for (const b of bias) {
-      if (b.commonName) {
-        identifiers.push(toIdentifier(b.commonName));
-        identifiers.push(toIdentifier(nameToUlt(b.commonName)));
-      }
-    }
-
-    for (const u of ult) {
-      if (u.commonName) {
-        identifiers.push(toIdentifier(u.commonName));
-        identifiers.push(toIdentifier(ultToName(u.commonName)));
-      }
-    }
-  }
-
-  // order by created ASC
-  const stmt = await db.prepare(`
-    SELECT * FROM labels
-    WHERE val IN ('${identifiers.map((ident) => ident).join("', '")}')
-    ORDER BY cts DESC
-    LIMIT 100
-  `);
-
-  const rows = await stmt.all<ComAtprotoLabelDefs.Label[]>();
-
-  const fans: Record<string, Set<string>> = {};
-  for (const row of rows.reverse()) {
-    const label = row.val;
-    const fan = row.uri;
-    const negate = row.neg;
-
-    if (!fans[label]) {
-      fans[label] = new Set();
-    }
-
-    if (negate) {
-      fans[label].delete(fan);
-    } else {
-      fans[label].add(fan);
-    }
-  }
-
-  // combine all the fans into a single list with label info
-  const allFans: Array<{label: string, did: string}> = [];
-  for (const [label, dids] of Object.entries(fans)) {
-    for (const did of Array.from(dids)) {
-      if (did !== message.senderDid) {
-        allFans.push({ label, did });
-      }
-    }
-  }
-
-  if (allFans.length === 0) {
+  if (!suggestion) {
     await conversation.sendMessage({
-      text: "No suggestions found. You're the first to like these groups!",
+      text: "No suggestions available right now"
     });
     return;
   }
-
-  // select a possible person at random from allFans
-  const random = allFans[Math.floor(Math.random() * allFans.length)]!;
-
-  const currentLabels =
-    (await getLabelerLabelDefinitions({
-      identifier: DID,
-      password: LABELER_PASSWORD,
-    })) || [];
-
-  const group = currentLabels.find((label) => label.identifier === random.label);
-
-  if (!group) {
-    console.error("Missing group from labeler", random.label);
-    await conversation.sendMessage({
-      text: "We found a suggestion, but for some reason, the group is missing from the labeler. Please try again.",
-    });
-    return;
-  }
-
-  const groupName = ultToName(group.locales?.[0]?.name ?? "a shared group");
-
-  // fetch did info from plc directory
-  const plcResult = await fetch(`https://plc.directory/${random.did}`);
-  const plcData = await plcResult.json() as DidDocument;
-  const handleUrl = plcData.alsoKnownAs?.find(aka => aka.startsWith("at://")) ?? "";
-  const handle = handleUrl.replace(/^at:\/\//, "");
 
   const outgoing = new RichText();
   outgoing.addText("Hey! Have you met ");
-  outgoing.addMention(`@${handle}`, random.did as At.DID);
+  outgoing.addMention(`@${suggestion.user.handle}`, suggestion.user.did as At.DID);
   outgoing.addText("? They're also a fan of ");
-  outgoing.addText(groupName);
+  outgoing.addText(suggestion.groupName);
   outgoing.addText("!");
 
   await conversation.sendMessage({
---
+++ src/actions/suggest.ts
@@ -0,0 +1,125 @@
+import dedent from "dedent";
+import { Action } from "./action.js";
+import { At, ComAtprotoLabelDefs } from "@atcute/client/lexicons";
+import { selectBias } from "../commonSql/selectBias.js";
+import { toIdentifier } from "../util/toIdentifier.js";
+import { nameToUlt, rowToLabel, ultToName } from "./add.js";
+import { doList } from "./list.js";
+import { db } from "../db.js";
+import { getLabelerLabelDefinitions } from "@skyware/labeler/scripts";
+import { DID, LABELER_PASSWORD } from "../constants.js";
+import { DidDocument } from "@atcute/client/utils/did";
+
+export const doSuggest = async (userDid: At.DID, suggestGroup?: string) => {
+  const identifiers: string[] = [];
+
+  if (suggestGroup && suggestGroup.length > 0) {
+    const result = await selectBias(suggestGroup);
+    identifiers.push(
+      toIdentifier(rowToLabel(result).name),
+      toIdentifier(rowToLabel(result, true).name)
+    );
+  } else {
+    // use users bias/ult list
+    // the "ult" version comes from the labeler, where we convert the en
+    // name to back to an identifier, turning the emoji back into dashes
+    // like "💖 ATEEZ" -> "--ATEEZ"
+    const { bias, ult } = await doList(userDid);
+    for (const b of bias) {
+      if (b.commonName) {
+        identifiers.push(toIdentifier(b.commonName));
+        identifiers.push(toIdentifier(nameToUlt(b.commonName)));
+      }
+    }
+
+    for (const u of ult) {
+      if (u.commonName) {
+        identifiers.push(toIdentifier(u.commonName));
+        identifiers.push(toIdentifier(ultToName(u.commonName)));
+      }
+    }
+  }
+
+  // order by created ASC
+  const stmt = await db.prepare(`
+    SELECT * FROM labels
+    WHERE val IN ('${identifiers.map((ident) => ident).join("', '")}')
+    ORDER BY cts DESC
+    LIMIT 100
+  `);
+
+  const rows = await stmt.all<ComAtprotoLabelDefs.Label[]>();
+
+  const fans: Record<string, Set<string>> = {};
+  for (const row of rows.reverse()) {
+    const label = row.val;
+    const fan = row.uri;
+    const negate = row.neg;
+
+    if (!fans[label]) {
+      fans[label] = new Set();
+    }
+
+    if (negate) {
+      fans[label].delete(fan);
+    } else {
+      fans[label].add(fan);
+    }
+  }
+
+  // combine all the fans into a single list with label info
+  const allFans: Array<{label: string, did: string}> = [];
+  for (const [label, dids] of Object.entries(fans)) {
+    for (const did of Array.from(dids)) {
+      if (did !== userDid) {
+        allFans.push({ label, did });
+      }
+    }
+  }
+
+  if (allFans.length === 0) {
+    return undefined;
+  }
+
+  // select a possible person at random from allFans
+  const random = allFans[Math.floor(Math.random() * allFans.length)]!;
+
+  const currentLabels =
+    (await getLabelerLabelDefinitions({
+      identifier: DID,
+      password: LABELER_PASSWORD,
+    })) || [];
+
+  const group = currentLabels.find((label) => label.identifier === random.label);
+
+  if (!group) {
+    console.error("Missing group from labeler", random.label);
+    return undefined;
+  }
+
+  const groupName = ultToName(group.locales?.[0]?.name ?? "a shared group");
+
+  // fetch did info from plc directory
+  const plcResult = await fetch(`https://plc.directory/${random.did}`);
+  const plcData = await plcResult.json() as DidDocument;
+  const handleUrl = plcData.alsoKnownAs?.find(aka => aka.startsWith("at://")) ?? "";
+  const handle = handleUrl.replace(/^at:\/\//, "");
+
+  return {
+    groupName,
+    user: {
+      did: random.did,
+      handle,
+    },
+  };
+}
+
+export const suggest: Action = {
+  match: /^\/suggest/,
+  cmd: "/suggest",
+  description: "Suggest someone to follow who shares a bias with you",
+  async handler(message, conversation, options) {
+    const suggestGroup = message.text.replace(suggest.match, "").trim();
+    const suggestion = await doSuggest(message.senderDid, suggestGroup);
+  },
+};
