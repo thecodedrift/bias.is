730998125a076a88cef0606e5f1caf63d8c29cee
Jakob Heuser <jakob@codedrift.com>
2024-12-20 17:32:26 -0800

    wip: Adds better handle resolution for suggestions

--- src/actions/admin/suggest.ts
+++ src/actions/admin/suggest.ts
@@ -5,7 +5,11 @@ import { nameToUlt, rowToLabel, ultToName } from "../add.js";
 import { toIdentifier } from "../../util/toIdentifier.js";
 import { doList } from "../list.js";
 import { db } from "../../db.js";
-import { ComAtprotoLabelDefs } from "@atcute/client/lexicons";
+import { At, ComAtprotoLabelDefs } from "@atcute/client/lexicons";
+import { RichText } from "@skyware/bot";
+import { getLabelerLabelDefinitions } from "@skyware/labeler/scripts";
+import { DID, LABELER_PASSWORD } from "../../constants.js";
+import { DidDocument } from "@atcute/client/utils/did";
 
 // search to single record to identifier
 // get random entry from sqllite with the label enabled (1)
@@ -54,7 +58,6 @@ export const suggest: AdminActionHandler = async (
     LIMIT 100
   `);
 
-  console.log(stmt.stmt);
   const rows = await stmt.all<ComAtprotoLabelDefs.Label[]>();
 
   const fans: Record<string, Set<string>> = {};
@@ -74,17 +77,58 @@ export const suggest: AdminActionHandler = async (
     }
   }
 
-  const debug = Object.entries(fans).map(([group, dids]) => {
-    return `${group}: ${Array.from(dids).join(", ")}`;
-  });
+  // combine all the fans into a single list with label info
+  const allFans: Array<{label: string, did: string}> = [];
+  for (const [label, dids] of Object.entries(fans)) {
+    for (const did of Array.from(dids)) {
+      if (did !== message.senderDid) {
+        allFans.push({ label, did });
+      }
+    }
+  }
+
+  if (allFans.length === 0) {
+    await conversation.sendMessage({
+      text: "No suggestions found. You're the first to like these groups!",
+    });
+    return;
+  }
+
+  // select a possible person at random from allFans
+  const random = allFans[Math.floor(Math.random() * allFans.length)]!;
+
+  const currentLabels =
+    (await getLabelerLabelDefinitions({
+      identifier: DID,
+      password: LABELER_PASSWORD,
+    })) || [];
+
+  const group = currentLabels.find((label) => label.identifier === random.label);
+
+  if (!group) {
+    console.error("Missing group from labeler", random.label);
+    await conversation.sendMessage({
+      text: "We found a suggestion, but for some reason, the group is missing from the labeler. Please try again.",
+    });
+    return;
+  }
+
+  const groupName = ultToName(group.locales?.[0]?.name ?? "a shared group");
+
+  // fetch did info from plc directory
+  const plcResult = await fetch(`https://plc.directory/${random.did}`);
+  const plcData = await plcResult.json() as DidDocument;
+  const handleUrl = plcData.alsoKnownAs?.find(aka => aka.startsWith("at://")) ?? "";
+  const handle = handleUrl.replace(/^at:\/\//, "");
+
+  const outgoing = new RichText();
+  outgoing.addText("Hey! Have you met ");
+  outgoing.addMention(`@${handle}`, random.did as At.DID);
+  outgoing.addText("? They're also a fan of ");
+  outgoing.addText(groupName);
+  outgoing.addText("!");
 
   await conversation.sendMessage({
-    text: dedent`
-      ${message.text}
-      Suggestions for ${message.senderDid}:
-      ${identifiers.join(", ")}
-      ---
-      ${debug.join("\n")}
-    `,
-  });
+    text: outgoing
+  })
 };
